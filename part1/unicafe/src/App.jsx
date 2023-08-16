94513e1eb7fe91fe17644e2208e598212d36d6fb
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-16 22:50:20 +0530

    feat: add components and initial code

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
+const Header = ({name}) => {
+  return <h1>{name}</h1>;
+}
+
+const Button = ({onClick, txt}) => {
+  <button onClick={onClick}>
+    {txt}
+  </button>
+}
+
 const App = () => {
+  const [clicksAll, setClicksAll] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+
+  const handleGoodClick = () => {
+    setClicksAll({...clicksAll, good: clicksAll.good + 1})
+  }
+
+  const handleNeutralClick = () => {
+    setClicksAll({...clicksAll, neutral: clicksAll.neutral + 1})
+  }
+
+  const handleBadClick = () => {
+    setClicksAll({...clicksAll, bad: clicksAll.bad + 1})
+  }
+
   return (
     <div>
-      <p>Hello world</p>
+      <Header name="Customer feedback" />
+      <Button txt="good" onClick={handleGoodClick} />
+      <Button txt="neutral" onClick={handleNeutralClick} />
+      <Button txt="bad" onClick={handleBadClick} />
+      <Header name="statistics" />
     </div>
   )
 }
