cd2470141806adc3e6abfea1ea0fd3d302e5e838
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-17 23:36:49 +0530

    feat: add name to the phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
+
 const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("button clicked", e.target);
+    setPersons((prevPersons) => [...prevPersons, { name: newName }]);
+  };
+
+  const handleInputChange = (e) => {
+    setNewName(e.target.value);
+  };
+
   return (
     <div>
-      <p>Hello world</p>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleClick}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, idx) => (
+          <li key={idx}>{person.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
