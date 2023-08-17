20752ef01d428e24a122230b5d8a75aab84e9f66
Anmol-Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-08-17 10:20:41 +0530

    feat: complete final structure with efficient code

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,9 +1,95 @@
-const App = () => {
+import PropTypes from 'prop-types';
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+Part.propTypes = {
+  part: PropTypes.string.isRequired,
+  exercises: PropTypes.number.isRequired,
+};
+
+const Content = ({ parts }) => {
   return (
     <div>
-      <p>Hello world</p>
+      {parts.map((part, index) => (
+        <Part key={index} part={part.name} exercises={part.exercises} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+Content.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+
+  return <p>Number of exercises: {totalExercises}</p>;
+};
+
+Total.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+const App = () => {
+
+  // const course = 'Half Stack application development'
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
